9e171c879101d71cd1400a418d161b17e9a0566c
Rich Wasek <rideburtonrw@gmail.com>
2021-10-21 20:51:48 -0600

    Add passing unit test

--- ngMyPantry/src/app/components/grocery-list/grocery-list.component.spec.ts
+++ ngMyPantry/src/app/components/grocery-list/grocery-list.component.spec.ts
@@ -1,25 +1,44 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GroceryListComponent } from './grocery-list.component';
 
-describe('GroceryListComponent', () => {
+fdescribe('GroceryListComponent', () => {
   let component: GroceryListComponent;
   let fixture: ComponentFixture<GroceryListComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GroceryListComponent ]
+      declarations: [ GroceryListComponent ],
+      imports: [RouterTestingModule],
+      providers: [HttpTestingController, HttpClientTestingModule, HttpClient, HttpHandler]
     })
     .compileComponents();
+
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GroceryListComponent);
     component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit', () => {
+    it('should call loadGroceryList', () => {
+      spyOn(component, 'loadGroceryList');
+      component.ngOnInit();
+
+      expect(component.loadGroceryList).toHaveBeenCalled();
+    });
   });
 });
